974bad107d0733c1c174a631fd1146869483515e
aldrin-kabya <128293805+aldrin-kabya@users.noreply.github.com>
2023-05-07 20:50:57 +0600

    Added form validation for empty fields

---
+++ add-proposals.php
@@ -0,0 +1,252 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>ResearchFlow | Research Management System</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link rel="stylesheet" href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="css/style.css">
+	
+	<!-- Pick date -->
+    <link rel="stylesheet" href="vendor/pickadate/themes/default.css">
+    <link rel="stylesheet" href="vendor/pickadate/themes/default.date.css">
+
+</head>
+
+<body>
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+    <?php
+		ob_start();
+        include('header.html');
+        include('sidebar.html');
+    ?>
+    	
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <!-- row -->
+            <div class="container-fluid">
+				    
+                <div class="row page-titles mx-0">
+                    <div class="col-sm-6 p-md-0">
+                        <div class="welcome-text">
+                            <h4>Add Proposal</h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Proposals</a></li>
+                            <li class="breadcrumb-item active"><a href="javascript:void(0);">Add Proposal</a></li>
+                        </ol>
+                    </div>
+                </div>
+				
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="card">
+							<div class="card-header">
+								<h4 class="card-title">Proposal Details</h4>
+							</div>
+							<div class="card-body">
+								<form action="add-proposals.php" method="post" name="proposalForm" onsubmit="return validateForm()">
+									<div class="row">
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Project Name</label>
+												<input type="text" name="project_name" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Department</label>
+												<input type="text" name="department" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-12 col-md-12 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Abstract</label>
+												<textarea name="abstract" class="form-control" rows="5"></textarea>
+											</div>
+										</div>
+										<div class="col-lg-12 col-md-12 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Proposal Details</label>
+												<textarea name="proposal_details" class="form-control" rows="5"></textarea>
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Starting Date</label>
+												<input name="starting_date" class="datepicker-default form-control" id="datepicker">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Project Duration</label>
+												<input type="text" name="project_duration" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Budget</label>
+												<input type="text" name="budget" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Professor Name</label>
+												<input type="text" name="professor_name" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Students/Collaborators</label>
+												<input type="text" name="collaborators" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-6 col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="form-label">Contact Number</label>
+												<input type="text" name="contact_number" class="form-control">
+											</div>
+										</div>
+										<div class="col-lg-12 col-md-12 col-sm-12">
+											<button type="submit" class="btn btn-primary" name="submit">Submit</button>
+											<button type="button" class="btn btn-light" onclick="history.back()">Cancel</button>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+            </div>
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+
+
+        <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p></p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+	<script src="vendor/global/global.min.js"></script>
+	<script src="vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="js/custom.min.js"></script>
+	<script src="js/dlabnav-init.js"></script>
+
+	<!-- Svganimation scripts -->
+    <script src="vendor/svganimation/vivus.min.js"></script>
+    <script src="vendor/svganimation/svg.animation.js"></script>
+	
+	<!-- pickdate -->
+    <script src="vendor/pickadate/picker.js"></script>
+    <script src="vendor/pickadate/picker.time.js"></script>
+    <script src="vendor/pickadate/picker.date.js"></script>
+	
+	<!-- Pickdate -->
+    <script src="js/plugins-init/pickadate-init.js"></script>
+
+
+	<script>
+		function validateForm() {
+			var projectName = document.forms["proposalForm"]["project_name"].value;
+			var department = document.forms["proposalForm"]["department"].value;
+			var abstract = document.forms["proposalForm"]["abstract"].value;
+			var proposalDetails = document.forms["proposalForm"]["proposal_details"].value;
+			var startingDate = document.forms["proposalForm"]["starting_date"].value;
+			var projectDuration = document.forms["proposalForm"]["project_duration"].value;
+			var budget = document.forms["proposalForm"]["budget"].value;
+			var professorName = document.forms["proposalForm"]["professor_name"].value;
+			var collaborators = document.forms["proposalForm"]["collaborators"].value;
+			var contactNumber = document.forms["proposalForm"]["contact_number"].value;
+
+			if (projectName == "" || department == "" || abstract == "" || proposalDetails == "" || startingDate == "" || projectDuration == "" || budget == "" || professorName == "" || collaborators == "" || contactNumber == "") {
+				alert("Please fill out all fields");
+				return false;
+			}
+		}
+	</script>
+
+	
+</body>
+</html>
+
+
+<?php
+if (isset($_POST['submit'])) {
+	// Retrieve the project name from the form data
+	$project_name = $_POST['project_name'];
+	$department = $_POST["department"];
+	$abstract = $_POST["abstract"];
+	$proposal_details = $_POST["proposal_details"];
+	$starting_date = $_POST["starting_date"];
+	$project_duration = $_POST["project_duration"];
+	$budget = $_POST["budget"];
+	$professor_name = $_POST["professor_name"];
+	$collaborators = $_POST["collaborators"];
+	$contact_number = $_POST["contact_number"];
+
+	// Connect to the MySQL database
+	include 'connection.php';
+
+	// Insert the project name into the database
+    $sql = "INSERT INTO proposals (project_name, department, abstract, proposal_details, starting_date, project_duration, budget, professor_name, collaborators, contact_number) VALUES ('$project_name', '$department', '$abstract', '$proposal_details', '$starting_date', '$project_duration', '$budget', '$professor_name', '$collaborators', '$contact_number')";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: all-proposals.php");
+        exit();
+    } else {
+        echo "Error adding record: " . mysqli_error($conn);
+    }
+
+	// Close the database connection
+	$conn->close();
+    ob_end_flush();
+}
+?>
